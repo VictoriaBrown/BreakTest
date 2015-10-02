00224997a9269d374e63a6477cbefd9a329ee9b0
Victoria Brown <victoriabrown0710@gmail.com>
2015-10-02 11:02:56 -0500

    Initial Commit

---
+++ BreakTest.java
@@ -0,0 +1,16 @@
+// BreakTest.java 10/2/15
+// break statement exiting a for statement.
+public class BreakTest {
+	public static void main(String[] args) {
+		int count; // control variable also used after loop terminates
+		
+		for (count = 1; count <= 10; count++) { // loop 10 times
+			if (count == 5) {
+				break; } // terminates loop if count is 5
+			
+			System.out.printf("%d ", count);
+		}
+		
+		System.out.printf("%nBroke out of loop at count = %d%n", count);
+	}
+}
